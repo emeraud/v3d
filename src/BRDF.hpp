0ff2b1a303fec72f3958c213717bd6d00b33d730
Valentin Emeraud <valentin.emeraud@gmail.com>
2014-12-07 20:49:03 +0100

    Fix BRDF

--- src/BRDF.hpp
+++ src/BRDF.hpp
@@ -15,23 +15,35 @@ class BRDF
                            const Material material,
                            const std::vector<Light>& lights,
                            Vec3Df &c) {
-    Vec3Df pointToViewer = camPos - objPos;
-    pointToViewer.normalize();
+      
+      Vec3Df pointToCam = camPos - objPos;
+      pointToCam.normalize();
 
-    for(unsigned int i=0; i<lights.size(); i++) {
-      Vec3Df pointToLight = lights[i].getPos() - objPos;
-      pointToLight.normalize();
+      c = Vec3Df(0.f, 0.f, 0.f);
 
-      Vec3Df reflectedPointToLight = (2 * Vec3Df::dotProduct(pointToLight, objNormal)) * objNormal - pointToLight;
-      reflectedPointToLight.normalize();
+      for (UInt i=0; i<lights.size(); i++) {
+        Vec3Df pointToLight = lights[i].getPos() - objPos;
+        pointToLight.normalize();
 
-      c = (material.getDiffuse() * Vec3Df::dotProduct(pointToLight, objNormal)
-                  + material.getSpecular() * pow(Vec3Df::dotProduct(reflectedPointToLight, pointToViewer), 20))
-              * material.getColor();
+        // diffuse part (calculate the cosine between normal and light direction)
+        float dot = Vec3Df::dotProduct(-objNormal,pointToLight);
+        dot = dot > 0.f ? dot : 0.f; // dot < 0: light at the wrong place
+        c += material.getColor() * lights[i].getColor() * dot * material.getDiffuse();
 
-      c = 255 * c;
+        // specular part
+        Vec3Df blinn = pointToLight + pointToCam;
+        float temp = Vec3Df::dotProduct(blinn,blinn);
+        if ((temp > 0.f && temp > EPSILON) || (temp < 0.f && temp < EPSILON)) {
+          temp = sqrt(temp);
+          blinn = (1.f/temp) * blinn;
+          float blinnTerm = Vec3Df::dotProduct(blinn,-objNormal);
+          blinnTerm = blinnTerm > 0.f ? blinnTerm : 0.f;
+          c += pow(blinnTerm,material.getSpecular()) * blinnTerm * lights[i].getColor();
+        }
+      }
+
+      c = c*255;
     }
-  }
 
   private:
     BRDF();
